24bc777069af95cb6e9d550e8cbfc4c66e8a2e6c
PREETI2096 <preetirg96@gmail.com>
2023-06-05 09:20:21 +0530

    Added : Hashtable program

---
+++ Hashtable/Hashtable/HashTable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashtable
+{
+    internal class HashTable<T>
+    {
+        private class Node
+        {
+            public T Data;
+            public Node Next;
+
+            public Node(T data)
+            {
+                Data = data;
+            }
+        }
+
+        private Node[] Names;
+
+        public HashTable(int size)
+        {
+            Names = new Node[size];
+        }
+    }
+}
